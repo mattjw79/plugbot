29fe66c9569f72862041355ee30bf753390ba0d7
Matt Whitney <mattjwhitney@gmail.com>
2018-09-24 06:56:08 -0400

    moved config structs/funcs to package

--- plugbot.go
+++ plugbot.go
@@ -3,34 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"strings"
 
+	"github.com/mattjw79/plugbot/config"
 	"github.com/nlopes/slack"
-	yaml "gopkg.in/yaml.v2"
 )
-
-// Config defines the configuration items parsed from the config file
-type Config struct {
-	APIToken string `yaml:"api_token"`
-	BotName  string `yaml:"bot_name"`
-}
-
-// Parse reads the config file and stores the values in the struct
-func (c *Config) Parse(filename string) error {
-	yamlFile, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return err
-	}
-
-	return yaml.Unmarshal(yamlFile, c)
-}
 
 // PlugBot defines the main bot structure
 type PlugBot struct {
 	ConfigFile string
-	Config     Config
+	Config     config.Config
 	API        *slack.Client
 	RTM        *slack.RTM
 	Info       *slack.Info
